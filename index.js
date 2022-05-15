e0830fd0a4b5c9bfab6bb5e61cd0abfbeca3b600
Kinjay Tantengco Ticsay <94281103+KinjayTicsay@users.noreply.github.com>
2022-05-16 03:30:21 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// alert('hello');
+function myFunction()
+
+if (condition) {
+    'If all both players got rocks, papers, and scissors will be both draws'
+}
+if (condition) {
+    'If P1 got rocks>scissor, papers>rock, and scissors>paper. P1 will be the winner'
+} else {
+    'If P2 got rocks>scissor, papers>rock, and scissors>paper. P2 will be the winner'
+}
+
+alert('TRY AGAIN')
+console.log('PAPER WINS')
